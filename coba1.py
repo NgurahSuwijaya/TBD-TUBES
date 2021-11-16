71f58c1b7da3639b2d7fadcdb751e2ef6589e321
RahSuwijaya <80748618+NgurahSuwijaya@users.noreply.github.com>
2021-11-16 21:45:10 +0800

    Update coba1.py

--- coba1.py
+++ coba1.py
@@ -1,4 +1,6 @@
 import tweepy
+import csv
+
 
 api_key = "Qi2fzEgqZt4WyllQJiihhHxSG"
 api_secret_key = "t5D7hWjoKr8kT7hK8Dr8bMgltDYRvCrcohuh0UYO7TnEZby7VZ"
@@ -9,7 +11,22 @@ auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret_key)
 api = tweepy.API(auth)
 
-resultSearch = api.search_tweets(q="Cryptocurrency", lang="id", count="100")
+keyparam="cryptopcurency"
+csvfile = open(keyparam+".csv","a+",newline="",encoding="utf-8")
+csvWriter = csv.writer(csvfile)
 
-for tweet in resultSearch:
-    print(tweet.user.screen_name, tweet.text )
+c = []
+i = []
+u = []
+t = []
+
+
+for tweet in tweepy.Cursor(api.search_tweets,q=keyparam, count=1000, lang="id").items():
+    print(tweet.created_at,tweet.id,tweet.user.name, tweet.text)
+    c.append(tweet.created_at)
+    i.append(tweet.id)
+    u.append(tweet.user.name)
+    t.append(tweet.text.encode("utf-8"))
+    tweetss = [tweet.created_at,tweet.id,tweet.user.name,tweet.text.encode("utf-8")]
+    csvWriter.writerow(tweetss)
+
